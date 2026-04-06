b7483f06d8268c975d3c37aeb442dd94b3ac1438
Nita Mario <164070849+NitaMario@users.noreply.github.com>
2026-04-06 06:49:25 +0300

    Add Basket.API Microservice (#3)
    * feat: add basketapi project
    
    * feat: shoppingcart and shoppingcartitem models
    
    * feat: add store, get, delete basket handler and endpoint
    
    * ci: add di for Carter and MediatR
    
    * feat: add repository pattern for basket
    
    * ci: add di for Marten
    
    * ci: add basketdb to docker images
    
    * refactor: modify get, delete, store basket methods to use repository pattern
    
    * ci: add di for customexceptionhandler
    
    * feat: add cache with repository
    
    * ci: add di for Redis and Scrutor
    
    * ci: add redis image to docker
    
    * feat: add health checks
    
    * ci: add basketapi to dockercompose
    
    * refactor: add CI build workflow for Basket.API

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using Carter;
+global using MediatR;
+global using Mapster;
+global using Marten;
+global using FluentValidation;
+global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Exceptions;
+global using Basket.API.Data;
