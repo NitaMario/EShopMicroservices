425419189438351af207aafc98054d86d74b5a6e
Nita Mario <164070849+NitaMario@users.noreply.github.com>
2026-04-07 08:03:27 +0300

    Add Ordering.Domain Layer (#5)
    * feat: configure structure for ddd pattern
    
    * refactor: add CI build workflow for Ordering.API
    
    * feat: add individual dependency injection
    
    * feat: add abstractions folder and implement abstractions
    
    * feat: add models, enums, valueobjects and globalusing
    
    * feat: add strongly typed ids for order entities
    
    * feat: add rich domain model and valueobjects, add domain exception
    
    * feat: add create, update events

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,18 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastracture;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastractureServices(builder.Configuration)
+    .AddApiServices();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.Run();
